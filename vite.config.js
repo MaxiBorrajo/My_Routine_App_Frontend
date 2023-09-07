a2d33df7bdad9d805d953512e20a66dbfdf305a2
MaxiBorrajo <maximilianoborrajo0@gmail.com>
2023-09-07 15:51:40 -0300

    adding pwa

--- vite.config.js
+++ vite.config.js
@@ -14,6 +14,8 @@ export default defineConfig({
         "favicon.ico",
         "apple-touch-icon.png",
         "safari-pinned-tab.svg",
+        "favicon-32x32.png",
+        "favicon-16x16.png"
       ],
       manifest: {
         name: "MyRoutineApp",
@@ -26,12 +28,12 @@ export default defineConfig({
         display: "standalone",
         icons: [
           {
-            src: "/android-chrome-192x192.png",
+            src: "./public/android-chrome-192x192.png",
             sizes: "192x192",
             type: "image/png",
           },
           {
-            src: "/android-chrome-512x512.png",
+            src: "./public/android-chrome-512x512.png",
             sizes: "512x512",
             type: "image/png",
           },
